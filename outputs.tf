cac48494d363bd6dfd9e299ed91b410c0d2f7506
Sam <beardmans72@gmail.com>
2019-12-27 17:13:02 +1300

    Correct outputs

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,21 @@
 output "id" {
     description = "Log Analytics Resource ID"
-    value = azurerm_log_analytics_solution.la_workspace.id
+    value = azurerm_log_analytics_workspace.la_workspace.id
 }
 
 output "workspace_id" {
     description = "Log Analytics Workspace Id"
-    value = azurerm_log_analytics_solution.la_workspace.workspace_id
-}
-
-output "portal_url" {
-    description = "Log Analytics Portal URL"
-    value = azurerm_log_analytics_solution.la_workspace.portal_url
+    value = azurerm_log_analytics_workspace.la_workspace.workspace_id
 }
 
 output "primary_shared_key" {
     description = "Log Analytics Workspace Primary Key"
-    value = azurerm_log_analytics_solution.la_workspace.primary_shared_key
+    value = azurerm_log_analytics_workspace.la_workspace.primary_shared_key
     sensitive = true
 }
 
 output "secondary_shared_key" {
     description = "Log Analytics Workspace Secondary Key"
-    value = azurerm_log_analytics_solution.la_workspace.secondary_shared_key
+    value = azurerm_log_analytics_workspace.la_workspace.secondary_shared_key
     sensitive = true
 }
